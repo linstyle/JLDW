0f8eee50ed894f95a192889f9626626abb4c47f1
lin_style <lin_style@foxmail.com>
2014-06-19 22:22:15 +0800

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<meta id="viewport" name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=2.0;" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>锦鲤大王</title>
+<link rel="stylesheet" type="text/css" media="screen" href="styles/basic.css" />
+</head>
+
+<body>
+<!-- head -->
+<div id="layHead">小金鱼的运气小屋</div>
+<hr size=1 style="color: #C0C0C0;border-style:dotted;width:100%">
+
+<!-- content -->
+
+
+<!-- bottom -->
+<div id="layFoot">愿望什么的就交给锦鲤们把，你努力就好。握鳍:)</div>
+</body>
+
+</html>
