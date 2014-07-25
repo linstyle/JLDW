73287562a31ddbfad11c72140b51e0aebf175ff3
lin_style <lin_style@foxmail.com>
2014-07-25 22:23:11 +0800

    4
    4

--- index.php
+++ index.php
@@ -11,11 +11,29 @@
 <!-- head -->
 <div id="layHead">小金鱼的运气小屋</div>
 <div style="text-align:center;color:#838383">恭迎第XX号 小金鱼 大人 </div>
-<button class="r-btn">运势</button>
+<div style="padding:0 0 0 10px"><button class="r-btn">运势</button></div>
 <hr size=1 style="color: #C0C0C0;border-style:dotted;width:100%">
 
 <!-- content -->
+<div style="text-align:center;color:#0000ff">今天</div>
+<div style="text-align:center;">财运: </div>
+<div style="text-align:center;">事业: </div>
+<div style="text-align:center;">健康: </div>
+<div style="text-align:center;">运气: </div>
 
+<div style="margin:20px;" </div>
+
+<div style="text-align:center;">宜: </div>
+<div style="text-align:center;">忌: </div>
+
+<div style="margin:20px;" </div>
+
+<div style="text-align:center;">幸运物 </div>
+<div style="text-align:center;">幸运色 </div>
+
+<div style="margin:20px;" </div>
+
+<div style="text-align:center;">增加明日运气方式</div>
 
 <!-- bottom -->
 <div id="layFoot">愿望什么的就交给锦鲤们把，你努力就好。握鳍:)</div>
