3972fe7a55e0b201e822c15344f517ed5254d323
linstyle <lin_style@foxmail.com>
2015-05-07 00:18:27 +0800

    sql

--- db/jinnang/back.sql
+++ db/jinnang/back.sql
@@ -21,15 +21,17 @@ USE `jldw`;
 DROP TABLE IF EXISTS `jinnang`;
 
 CREATE TABLE `jinnang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `getType` varchar(10) DEFAULT NULL,
   `name` varchar(12) DEFAULT NULL,
   `sex` int(11) DEFAULT NULL COMMENT '1是男，0是女',
-  `bornTime` varchar(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `bornTime` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 /*Data for the table `jinnang` */
 
-insert  into `jinnang`(`getType`,`name`,`sex`,`bornTime`) values ('my','æž—ä¸œå¹³',1,'2015.10'),('my','æž—ä¸œå¹³',1,'2015.10'),('my','æž—ä¸œå¹³',1,'2015.10'),('my','æž—ä¸œå¹³',1,'2015.10'),('my','æž—ä¸œå¹³',1,'2015.10'),('my','æž—ä¸œå¹³',1,'123123'),('my','æž—ä¸œå¹³',1,'123123'),('my','æž—ä¸œå¹³',1,'123123'),('my','èŒƒå¾·è¨èŒƒ',1,'234234');
+insert  into `jinnang`(`id`,`getType`,`name`,`sex`,`bornTime`) values (9,'s第三方','林东平',1,'1995.10'),(10,'s第三方','林东平',1,'1995.10'),(11,'s第三方','林东平',1,'1995.10'),(12,'my','林东平',1,'1995.10'),(13,'my','林东平',1,'1995.10'),(14,'my','林东平',1,'1995.10'),(15,'my','林东平',1,'1995.10'),(16,'my','林东平',1,'1995.10'),(17,'my','锦鲤大王',1,'1800.1');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
